7ad1af8eb02dea3264cfa83d2933e056f35a873f
Robert Warren <grindnmosh@gmail.com>
2013-02-17 22:04:46 -0700

    project 1

---
+++ Warren_Robert_Assignment1.js
@@ -0,0 +1,57 @@
+//alert("JavaScript works!");
+
+/*
+Robert Warren
+02/06/2013
+Project 1
+Teen Super Sleuth
+*/
+
+//my variables
+var myFriend = 'Sam';
+var locale = 'gym\'s locker room.';
+var success = 'stay hidden and catch the thief in the act.';
+var failure = 'I was unable to ' + success;
+var team = 2;
+var hidden = true;
+var thieves = 'people working as a team';
+var numOfThieves = 5
+
+//my first outputs
+	console.log('There is a thief stealing from people in the ' + locale)
+	console.log('I need to figure out who is doing it and solve the mystery.')
+	console.log('This would be easier with 2 people, so I will ask ' + myFriend + '.')
+	
+//My Conditionals
+if (team <= 1) {
+	console.log('Sam\'s answer is no, therefore he can not be trusted. This will be a ' + alone + ' man mission.');
+	if (team = team - 1, hidden === true) {
+		console.log('I was able to' + success)
+		console.log('I was not able to get a clear look to identify the people I saw')
+		console.log('There were 4-5 people on the other side of ' + locale)
+		console.log('This was not the best hiding spot as I was unable to see who it was.')
+		console.log('I will have to come up with a better plan and try again.');
+		;
+	} else {
+		console.log(failure)
+		console.log('I will try again to ' + success)
+		console.log('I will do this alone, but 2 would be better than 1.');
+	};
+} else {
+		console.log(myFriend + ' has agreed to help. ')
+		console.log('I will ask him to cover for me while I hide in the ' + locale);
+	if (numOfThieves === 5, thieves === 'people working as a team', numOfThieves - team >= 1) {
+		console.log('Sam was able to cover the fact I was missing')
+		console.log('I carefully watched and saw the movement and someone messing with the lockers.')
+		console.log('There were ' + numOfThieves + ' people on the other side of ' + locale)
+		console.log('This was not the best hiding spot as I was unable to see who it was.')
+		console.log('I will have to come up with a better plan and try again.');
+		console.log('Maybe ' + myFriend + ' has some ideas on what to do next.');
+	} else {
+		console.log('Sam was unable to cover for me and therefore ' + failure)
+		console.log('I will try again to ' + success)
+		console.log('Maybe ' + myFriend + ' has some ideas we can discuss to catch these thieves.');
+		};
+};
+	
+
